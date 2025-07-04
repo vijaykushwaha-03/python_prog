fe78e8fd0ef474c13cebc680547e3dbeaf0b65fb
Vijay Kushwaha <vijaykushwaha868856@gmail.com>
2025-07-04 23:43:08 +0530

    first commit

---
+++ basic_programs.py
@@ -0,0 +1,36 @@
+def reverse_string(s):
+    return s[::-1]
+
+print(reverse_string("hello"))  # olleh
+
+def is_palindrome(s):
+    return s == s[::-1]
+
+print(is_palindrome("level"))  # True
+print(is_palindrome("hello"))  # False
+
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    return n * factorial(n - 1)
+
+print(factorial(5))  # 120
+
+
+def find_largest(nums):
+    largest = nums[0]
+    for num in nums:
+        if num > largest:
+            largest = num
+    return largest
+
+print(find_largest([1, 8, 3, 4]))  # 8
+
+
+def fibonacci(n):
+    fib = [0, 1]
+    for i in range(2, n):
+        fib.append(fib[i - 1] + fib[i - 2])
+    return fib[:n]
+
+print(fibonacci(5))  # [0, 1, 1, 2, 3]
